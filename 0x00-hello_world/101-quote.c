6a7ce1d91b45e57c48b78163e95b0725a90c5249
eya-98 <eya.nani@sesame.com.tn>
2020-09-16 21:06:59 +0000

    m

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,6 @@
 */
 int main(void)
 {
-(sort 'and that piece of art is useful" - Dora Korpar, 2015-10-19\n' > file1;)
-file1 *stderr;
-cat file1;
+(gcc 'and that piece of art is useful" - Dora Korpar, 2015-10-19\n';)
 return (1);
 }
