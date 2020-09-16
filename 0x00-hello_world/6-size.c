03e8064a90b9206e76a04e22a0b8b6d4222fb148
eya-98 <eya.nani@sesame.com.tn>
2020-09-16 17:02:39 +0000

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,15 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
-* main - Prints size
-* Description: prints size
-* Return: 0
+*main - function
+*Description: prints sizes
+*Return: 0
 */
 int main(void)
 {
 printf("Size of a char: %i byte(s)\n", sizeof(char));
 printf("Size of an int: %i byte(s)\n", sizeof(int));
-printf("Size of a longint: %i byte(s)\n", sizeof(long int));
-printf("Size of a longlongint: %i byte(s)\n", sizeof(long long int));
+printf("Size of a long int: %i byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
 printf("Size of a float: %i byte(s)\n", sizeof(float));
 return (0);
 }
