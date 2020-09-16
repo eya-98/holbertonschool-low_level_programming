c9a4d877787dda8241cd37cf1d5380dbd6143203
eya-98 <eya.nani@sesame.com.tn>
2020-09-16 16:53:19 +0000

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,9 +8,8 @@ int main(void)
 {
 printf("Size of char: %i byte\n", sizeof(char));
 printf("Size of int: %i bytes\n", sizeof(int));
-printf("Size of float: %i bytes\n", sizeof(float));
-printf("Size of double: %i bytes\n", sizeof(double));
 printf("Size of longint: %i bytes\n", sizeof(long int));
 printf("Size of longlongint: %i bytes\n", sizeof(long long int));
+printf("Size of float: %i bytes\n", sizeof(float));
 return (0);
 }
