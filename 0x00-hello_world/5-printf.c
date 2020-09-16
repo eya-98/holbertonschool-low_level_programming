a541bcdb24cb92c2b14241dcf70fb06c958eca3d
eya-98 <eya.nani@sesame.com.tn>
2020-09-16 16:04:14 +0000

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 /**
  * main - Prints string
- * Description: Prints "\"Programming is like building a multilingual puzzle"
+ * Description: Prints "\"with proper grammar, but the outcome is a piece of art"
  * Return: 0
  */
 int main(void)
 {
-  printf("\"Programming is like building a multilingual puzzle");
+  printf("\"with proper grammar, but the outcome is a piece of art");
   return (0);
 }
