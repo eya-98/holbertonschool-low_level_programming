3983b1941687e3d80aaebe34224f5357ce1deb12
eya-98 <eya.nani@sesame.com.tn>
2020-09-16 16:07:13 +0000

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 /**
- * main - Prints string
- * Description: Prints "\"with proper grammar, but the outcome is a piece of art"
- * Return: 0
- */
+* main - Prints string
+* Description: Prints "with proper grammar, but the outcome is a piece of art"
+* Return: 0
+*/
 int main(void)
 {
-  printf("\"with proper grammar, but the outcome is a piece of art,");
-  return (0);
+printf("\"with proper grammar, but the outcome is a piece of art,");
+return (0);
 }
