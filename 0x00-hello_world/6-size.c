90246843336594fe5e36b0805f884bf3368e1a53
eya-98 <eya.nani@sesame.com.tn>
2020-09-16 16:50:39 +0000

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,16 @@
 #include<stdio.h>
-int main(void) {
-printf("Size of char: %ld byte\n", sizeof(charType));
-printf("Size of int: %ld bytes\n", sizeof(intType));
-printf("Size of float: %ld bytes\n", sizeof(floatType));
-printf("Size of double: %ld bytes\n", sizeof(doubleType));
-printf("Size of longint: %ld bytes\n", sizeof(longintType));
-printf("Size of longlongint: %ld bytes\n", sizeof(longlongintType));
-return 0;
+/**
+* main - Prints size
+* Description: prints size
+* Return: 0
+*/
+int main(void)
+{
+printf("Size of char: %i byte\n", sizeof(char));
+printf("Size of int: %i bytes\n", sizeof(int));
+printf("Size of float: %i bytes\n", sizeof(float));
+printf("Size of double: %i bytes\n", sizeof(double));
+printf("Size of longint: %i bytes\n", sizeof(long int));
+printf("Size of longlongint: %i bytes\n", sizeof(long long int));
+return (0);
 }
