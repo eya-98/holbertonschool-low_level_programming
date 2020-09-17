ca166d41f9712be8273bb5bec6659a399fc8b0d0
eya-98 <eya.nani@sesame.com.tn>
2020-09-17 18:27:34 +0000

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints out all numbers under 10
+ * description: random and last digit
+ * Return: Always 0
+ */
+int main(void)
+{
+char number;
+for (number = '48'; number < '58'; number++)
+putchar(number);
+putchar("\n");
+return (0);
+}
