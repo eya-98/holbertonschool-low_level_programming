c243336274928f5853ca410d1fb5138ec2d228f5
eya-98 <eya.nani@sesame.com.tn>
2020-09-17 17:13:15 +0000

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - main function
+* description: random and last digit
+* Return: Always 0
+*/
+void main (void) {
+char letter;
+letter = 'a';
+for (letter <= 'z')
+putchar (letter);
+putchar ('\n');
+return (0); 
