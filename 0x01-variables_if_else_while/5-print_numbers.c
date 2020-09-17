0ae7ffa9f7f14cc8fcc001e6b4780837d576ba1d
eya-98 <eya.nani@sesame.com.tn>
2020-09-17 18:15:49 +0000

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints out all numbers under 10
+ * description: random and last digit
+ * Return: Always 0
+ */
+int main(void)
+{
+int number;
+for (number = 0; number < 10; number++)
+printf("%d",number);
+printf("\n");
+return (0);
+}
