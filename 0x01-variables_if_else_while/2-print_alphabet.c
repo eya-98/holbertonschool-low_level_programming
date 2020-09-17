2bf1dda2d6db1e6c65ead968e125b9b556b4fd11
eya-98 <eya.nani@sesame.com.tn>
2020-09-17 17:24:32 +0000

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,13 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 /**
 * main - main function
 * description: random and last digit
 * Return: Always 0
 */
-void main (void) {
+int main(void) {
 char letter;
-letter = 'a';
-for (letter <= 'z')
-putchar (letter);
-putchar ('\n');
-return (0); 
+for (letter = 'a'; letter <= 'z'; c++)
+putchar(letter);
+ putchar ('\n');
+return (0);
+}
