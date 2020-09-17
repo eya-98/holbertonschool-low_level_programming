e2a98555991578e38eeb1ff59ae32e4bdd889df0
eya-98 <eya.nani@sesame.com.tn>
2020-09-17 18:36:24 +0000

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * description: random and last digit
+ * Return: Always 0
+ */
+int main(void)
+{
+char letter;
+for (letter = 'z'; letter <= 'a'; letter--)
+putchar(letter);
+putchar ('\n');
+return (0);
+}
