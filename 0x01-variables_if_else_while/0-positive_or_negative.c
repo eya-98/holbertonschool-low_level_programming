e9c80b30bb7b35fd03590d01ae9b0708c74bc08c
eya-98 <eya.nani@sesame.com.tn>
2020-09-17 15:48:03 +0000

    0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,10 +12,16 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
-printf(" %d is positive\n", n);
+{
+printf("%d is positive\n", n);
+}
 else if (n == 0)
-printf(" %d is zero\n", n);
-else if (n < 0)
-printf(" %d is negative\n", n);
+{
+printf("%d is zero\n", n);
+}
+else (n < 0)
+{
+printf("%d is negative\n", n);
+}
 return (0);
 }
