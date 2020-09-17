1afcc4bda8af2c408c74379aacdfe26124ea6ec1
eya-98 <eya.nani@sesame.com.tn>
2020-09-17 16:23:24 +0000

    1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,9 +11,9 @@
 int main(void)
 {
 int n, a;
-a = n % 10
 srand(time(0));
 n = rand() - RAND_MAX / 2;
+ a = n % 10;
 if (a > 5)
 {
 printf("Last digit of %d is %f and is and is greater than 5 /n", n, a);
