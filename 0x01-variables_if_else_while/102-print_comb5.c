958c60cdb3129d1afb72a20b27244c5403521727
eya-98 <eya.nani@sesame.com.tn>
2020-09-18 23:29:44 +0000

    102

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+/**
+ * main - main function
+ *
+ * Description: prints numbers from 01 to 89
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+int d, j, m, n;
+for (d = '0' ; d <= '9'; d++)
+{
+for (j = '0' ; j = '8'; j++)
+{
+for (m = '0'; m <= '9'; m++)
+{
+for (n = '0'; n = '9'; n++)
+{
+putchar(d);
+putchar(j);
+putchar (' ');
+putchar(m);
+putchar (n);
+if ((d != '9') || (j != '8') || (m != '9') || (n != '9'))
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
