6be42f82ce6add0c9a8acd9164cae001ea22f42f
eya-98 <eya.nani@sesame.com.tn>
2020-09-22 17:45:31 +0000

    hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * jack_bauer - prints every minute of the day
+ *
+ */
+void jack_bauer (void)
+{
+int i, j;
+for ( i = 0 ; i <= 23 ; i++ )
+{
+for ( j= 0 ; j <= 59 ; j++ )
+{
+_putchar('0'+ i / 10);
+_putchar('0' + i % 10);
+_putchar(':');
+_putchar('0'+ j / 10);
+_putchar('0' + j % 10);
+_putchar('\n');
+}
+}
+}
