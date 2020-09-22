c3f930365f4d17a1d7baac3b2e19e1e28ac46e86
eya-98 <eya.nani@sesame.com.tn>
2020-09-22 17:49:42 +0000

    hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -3,17 +3,17 @@
  * jack_bauer - prints every minute of the day
  *
  */
-void jack_bauer (void)
+void jack_bauer(void)
 {
 int i, j;
-for ( i = 0 ; i <= 23 ; i++ )
+for (i = 0 ; i <= 23 ; i++)
 {
-for ( j= 0 ; j <= 59 ; j++ )
+for (j = 0 ; j <= 59 ; j++)
 {
-_putchar('0'+ i / 10);
+_putchar('0' + i / 10);
 _putchar('0' + i % 10);
 _putchar(':');
-_putchar('0'+ j / 10);
+_putchar('0' + j / 10);
 _putchar('0' + j % 10);
 _putchar('\n');
 }
