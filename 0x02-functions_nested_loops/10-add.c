35f531fcd6a47f7b0f4abd6403448c22d1fb58e9
eya-98 <eya.nani@sesame.com.tn>
2020-09-23 16:33:52 +0000

    10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * add - adds two integers
+ *@a : first integer
+ *@b : seconf integer
+ * Return: Always 0.
+ */
+int add(int a, int b)
+{
+int c;
+c = a + b;
+_putchar('0' + c);
+return (c);
+}
