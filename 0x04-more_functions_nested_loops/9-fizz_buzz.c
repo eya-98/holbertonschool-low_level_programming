585bc5d773a9119e6198652542959c1dac4df8bb
eya-98 <eya.nani@sesame.com.tn>
2020-09-24 22:29:22 +0000

    fizz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+B#include <stdio.h>
+/**
+ *main  - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int i, j, k, l;
+for (i = 1 ; i <= 100 ; i++)
+{
+k = i % 3;
+j = i % 5;
+l = i % 15;
+if (l == 0)
+printf("FiizBuzz ");
+else if (k == 0)
+printf("Fizz ");
+else if (i == 100)
+printf("Buzz\n");
+else if (j == 0)
+printf("buzz ");
+else
+printf("%d ", i);
+}
+return (0);
+}
