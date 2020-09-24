0f4feee4dda1317884501924e46dbb38b0cf57e4
eya-98 <eya.nani@sesame.com.tn>
2020-09-24 19:08:55 +0000

    fixed

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ * print_square - main function
+ *
+ *@size : integer
+ *
+ *
+ *Return: void
+ *
+ */
+void print_square(int size)
+{
+int i, j;
+if (size > 0)
+{
+for (i = 1; i <= size; i++)
+{
+for (j = 1; j <= size; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
+}
