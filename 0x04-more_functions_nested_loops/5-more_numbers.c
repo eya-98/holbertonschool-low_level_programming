be3cdf56522bfec06b602de121228ee83308743c
eya-98 <eya.nani@sesame.com.tn>
2020-09-24 17:17:21 +0000

    more_numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,21 +6,14 @@
  */
 void more_numbers(void)
 {
-int i, j, k;
-for (i = 0 ; i < 10 ; i++)
+int i, j;
+for (i = 0; i < 10; i++)
 {
-for (j = 0 ; j <= 14 ; j++)
+for (j = 0; j < 15; j++)
 {
-k = j;
-if (k >= 10)
-{
-k = j / 10;
-_putchar ('0' + k);
-}
-if (k < 10)
-{
-putchar ('0' + j % 10);
-}
+if (j >= 10)
+_putchar (j / 10 + '0');
+_putchar (j % 10 + '0');
 }
 _putchar ('\n');
 }
