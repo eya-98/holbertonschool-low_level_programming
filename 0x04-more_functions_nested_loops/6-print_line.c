fe94818b1442e2f4820a2ad103eef8f230aad5bd
eya-98 <eya.nani@sesame.com.tn>
2020-09-24 18:37:13 +0000

    lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * void print_line - check the code for Holberton School students.
+ * @n : integer
+ * Return: Always 0.
+ */
+void print_line(int n)
+{
+int i;
+if (n > 0)
+{
+for (i = 0 ; i <= n ; i++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
+else
+{
+_putchar('\n');
+}
+}
