9dd6fe51217e67d70636c8e62f451dbc8565c761
eya-98 <eya.nani@sesame.com.tn>
2020-09-26 15:07:40 +0000

    prime factor

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -7,21 +7,20 @@
 int main(void)
 {
 unsigned long int n;
-int i, max;
+int i, max, j;
 n = 612852475143;
-while ((n % 2) == 0)
-{
-max = 2;
-n = n / 2;
-}
 for (i = 2; n; i++)
 {
 while ((n % i) == 0)
 {
 n = n / i;
-max = i;
+max = 1;
+for (j = 2; i / 2; i++)
+if (i % j == 0)
+max = 0;
 }
 }
+if (max == 1)
 printf("%d\n", max);
 return (0);
 }
