548bd92aface76b100418ea9d18c734b4d1b6a3e
eya-98 <eya.nani@sesame.com.tn>
2020-09-27 10:40:11 +0000

    time table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_times_table - check the code for Holberton School students.
+ * @n : int 
+ * Return: Always 0.
+ */
+void print_times_table(int n)
+{
+int i, j, l;
+if ((n < 0) || (n > 15))
+{
+_putchar('\n');
+}
+else
+{
+for (i = 0; i <= n ; i++) 
+{
+for (j = 0; j <= n; j++)
+{
+l = i * j;
+if (j != 0)
+{
+if (l < 10)
+printf(",   %d", l);
+else if ( l < 100)
+printf(",  %d", l);
+else 
+printf(", %d", l);
+}
+else
+printf("%d", l);
+}
+printf("\n");
+}
+}
+} 
