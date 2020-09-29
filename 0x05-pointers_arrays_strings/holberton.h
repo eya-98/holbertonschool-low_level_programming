f41faf30628fcf5a251b8003436f7cc0b3555939
eya-98 <eya.nani@sesame.com.tn>
2020-09-29 20:18:07 +0000

    fixed

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -2,6 +2,7 @@
 #define HOLBERTON_H
 void swap_int(int *a, int *b);
 void reset_to_98(int *n);
+int _putchar(char c);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
@@ -10,4 +11,5 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+
 #endif
