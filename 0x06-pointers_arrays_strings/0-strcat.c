2421ac4de1b4f6138b56693caea1cc7946242917
eya-98 <eya.nani@sesame.com.tn>
2020-10-01 13:20:44 +0000

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * *_strcat - check the code for Holberton School students.
+ * @dest : char;
+ * @src : char;
+ * Return: Always 0.
+ */
+char *_strcat(char *dest, char *src)
+{
+  int i = 0, j;
+ while (dest[i] != '\0')
+   {i++;}
+for (j = 0; src[j] != '\0'; j++)  
+{
+  dest[i] = src[j];
+ i++;
+}
+return(dest);
+}
