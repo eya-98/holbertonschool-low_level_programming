91521e3016726281a008e758dc54ebe39dd619f7
eya-98 <eya.nani@sesame.com.tn>
2020-10-01 13:39:50 +0000

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,13 +7,15 @@
  */
 char *_strcat(char *dest, char *src)
 {
-  int i = 0, j;
- while (dest[i] != '\0')
-   {i++;}
-for (j = 0; src[j] != '\0'; j++)  
+int i = 0, j;
+while (dest[i] != '\0')
 {
-  dest[i] = src[j];
- i++;
+i++;
 }
-return(dest);
+for (j = 0; src[j] != '\0'; j++)
+{
+dest[i] = src[j];
+i++;
+}
+return (dest);
 }
