fb8e8fc64c9cd79014fb78b27f671667e0f5cd71
eya-98 <eya.nani@sesame.com.tn>
2020-10-02 14:53:23 +0000

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+/**
+* *leet - check the code for Holberton School students.
+* @a : char
+* Return: Always 0.
+*/
+char *leet(char *a)
+{
+int i, k;
+char s[10];
+char m[10];
+s[0] = 'a';
+s[1] = 'A';
+m[0] = '4';
+m[1] = '4';
+s[2] = 'e';
+s[3] = 'E';
+m[2] = '3';
+m[3] = '3';
+s[4] = 'o';
+s[5] = 'O';
+m[4] = '0';
+m[5] = '0';
+s[6] = 't';
+s[7] = 'T';
+m[6] = '7';
+m[7] = '7';
+s[8] = 'l';
+s[9] = 'L';
+for (i = 0; a[i] != '\0'; i++)
+for (k = 0; s[k] != '\0'; k++)
+if (a[i] == s[k])
+{
+a[i] = m[k];
+}
+return (a);
+}
