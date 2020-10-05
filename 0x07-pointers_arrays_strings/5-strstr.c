fe02ea31cde42664f0042265b3e36314d7a1a677
eya-98 <eya.nani@sesame.com.tn>
2020-10-05 17:42:29 +0000

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -3,14 +3,14 @@
  * *_strstr -  a function that locates a substring.
  * @haystack : char
  * @needle : char
- * Return: Always 0.
+ * Return: a pointer to the beginning of the located substring or 0.
  */
 char *_strstr(char *haystack, char *needle)
 {
 int i;
 for (i = 0; haystack[i] != '\0'; i++)
 {
-if (haystack[i] == needle[0])
+if (haystack[i] == *needle)
 return (haystack + i);
 }
 return ('\0');
