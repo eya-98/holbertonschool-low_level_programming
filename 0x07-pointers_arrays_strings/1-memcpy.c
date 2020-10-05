5b0a195db78cdb371122915f12f7f1e8ebf0df72
eya-98 <eya.nani@sesame.com.tn>
2020-10-05 00:29:27 +0000

    1-memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * *_memcpy - prints buffer in hexa
+ * @dest: destination var
+ * @src: source var
+ * @n : unsigned int n
+ * Return: Nothing.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i;  
+for (i = 0; i <= n; i++)
+{
+dest[i] = src[i];
+}
+return(dest);
+}
