aae690accac59f0107db8aaed542b5c6897c39e1
eya-98 <eya.nani@sesame.com.tn>
2020-10-05 18:33:18 +0000

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,12 +8,12 @@
 char *_strstr(char *haystack, char *needle)
 {
 int j;
-for (j = 0; haystack[j] > '\0'; j++)
+for (j = 0; haystack[j] != '\0'; j++)
 {
 if (haystack[j] == needle[0])
 {
+return (haystack + j - 1);
+}
+}
 return (haystack + j);
-}
-}
-return (haystack + 1);
 }
