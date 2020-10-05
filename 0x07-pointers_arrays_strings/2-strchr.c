8fbc7742a3fc3b011f839111af895aa328e3b7c1
eya-98 <eya.nani@sesame.com.tn>
2020-10-05 01:23:21 +0000

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * *_strchr -  locates a character in a string
+ * @s : string
+ * @c : char
+ * Return: 0 or the occurence of c.
+ */
+char *_strchr(char *s, char c)
+{
+while (*s != '\0')
+{
+if (*s == c)
+{
+return (s);
+}
+s++;
+}
+return (s + 1);
+}
