e56dc67be696aff15f9a56eed011037deeac8685
eya-98 <eya.nani@sesame.com.tn>
2020-10-05 14:33:34 +0000

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,13 +7,14 @@
  */
 char *_strchr(char *s, char c)
 {
-while (*s != '\0')
+int i = 0;
+while (s[i] != '\0')
 {
-if (*s == c)
+if (s[i] == c)
 {
-return (s);
+return (s + i);
 }
-s++;
+i++;
 }
-return (NULL);
+return (s + i + 1);
 }
