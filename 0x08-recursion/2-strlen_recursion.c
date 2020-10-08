b5d86edd9a0d9a4eeeb3460fa1fc05e187f87784
eya-98 <eya.nani@sesame.com.tn>
2020-10-08 21:38:27 +0000

    strlen

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * _strlen_recursion - a function that returns the length of a string.
+ *
+ * Return: Always 0.
+ */
+int _strlen_recursion(char *s)
+{
+int n = 0;
+if (*s != '\0')
+{
+n = _strlen_recursion(s + 1);
+n++;
+} 
+return (n);
+}
