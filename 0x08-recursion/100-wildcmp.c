9dacc6c11a979cd4bd6ad0e85933b4919c237a35
eya-98 <eya.nani@sesame.com.tn>
2020-10-08 22:49:23 +0000

    wildcmp

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -7,18 +7,15 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-int i = 0;
-int j, k;
+if (!(*s1))
 {
+if (!(*s2))
+return (1);
 if (*s2 == '*')
 return (wildcmp(s1, s2 + 1));
 }
-if (*s1 == *s2 && *s1 != '\0')
+if (*s1 == *s2)
 return (wildcmp(s1 + 1, s2 + 1));
-if (*s1 == '\0')
-{
-if (*s2 == '\0')
-return (1);
 if (*s2 == '*')
 return ((wildcmp(s1 + 1, s2)) || (wildcmp(s1, s2 + 1)));
 return (0);
