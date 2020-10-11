f8567fa00ae43a818e8398de6c6137311650daac
eya-98 <eya.nani@sesame.com.tn>
2020-10-11 00:24:12 +0000

    print name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/*
+ * main - a program that prints its name
+ * @argc : int
+ * @argc : string
+ */
+int main(int argc, char *argv[])
+{
+if (argc != 0)
+{
+printf("%s\n", argv[0]);
+}
+return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+ARGV and ARGC
