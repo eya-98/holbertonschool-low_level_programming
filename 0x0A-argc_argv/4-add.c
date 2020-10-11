0e24de10a7ebcc5f5ce4e273f599ba98fd8b2a46
eya-98 <eya.nani@sesame.com.tn>
2020-10-11 18:42:20 +0000

    add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - a program that prints its name
+ * @argc : int
+ * @argv : string
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+int i, j, k = 0;
+for (i = 1; i < argc; i++)
+{
+for (j = 0; argv[i][j] != '\0'; j++)
+{
+if (argv[i][j] <= '0' && argv[i][j] >= '9')
+{
+printf("Error\n");
+return (1);
+}
+}
+k = k + atoi(argv[i]);
+}
+printf("%d\n", k);
+return (0);
+}
