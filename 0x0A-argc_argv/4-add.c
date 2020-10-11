a691858f14bb6e39218c0b79dc8cbeb45fc8769f
eya-98 <eya.nani@sesame.com.tn>
2020-10-11 19:19:06 +0000

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,7 +13,7 @@ for (i = 1; i < argc; i++)
 {
 for (j = 0; argv[i][j] ; j++)
 {
-if (argv[i][j] < '0' && argv[i][j] > '9')
+if (argv[i][j] < '0' || argv[i][j] > '9')
 {
 printf("Error\n");
 return (1);
