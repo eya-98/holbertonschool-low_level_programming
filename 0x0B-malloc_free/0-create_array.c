87b58db39bbc6dcf1cb3cda16601393806f27efd
eya-98 <eya.nani@sesame.com.tn>
2020-10-16 14:42:57 +0000

    create

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "holberton.h"
+/**
+ **create_array - creates an array of chars
+ * @c : string
+ * @size: the size of the memory to print
+ * Return: Nothing.
+ */
 char *create_array(unsigned int size, char c)
 {
 char *s;
@@ -9,19 +15,17 @@ if (size == 0)
 return (0);
 else
 {
-  s = malloc(sizeof(char) * size);
-{
+s = malloc(sizeof(char) * size);
 for (i = 0; i <= size; i++)
 {
 s[i] = c;
 }
-if (s[size] == '\0')
+if (s[0] == '\0')
 {
 return (0);
 }
 else
 return (s);
-}
 }
 return (0);
 }
