6d17edbfc6bc385dc8364b891f8e3f331ea99007
eya-98 <eya.nani@sesame.com.tn>
2020-10-17 09:04:15 +0000

    str concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,33 +10,35 @@
 char *str_concat(char *s1, char *s2)
 {
 int i, j, k;
- char *s3;
+char *s3;
+if (s1 == 0)
+{
+s1 = " ";
+}
 for (i = 0; s1[i] != '\0'; i++)
 {
 }
+if (s2 == 0)
+{
+s2 = " ";
+}
 for (j = 0 ; s2[j] != '\0'; j++)
 {
-s1[i+1] = s2[j];
-i++;
 }
-s1[i + 1] = '\0';
 s3 = malloc(sizeof(char) * (i + j + 1));
 if (s3 == 0)
+{
 return (0);
-else
-{
-if (s1[0] == '\0')
-s3 = '\0';
-else
-{
-for (k = 0; s1[k] != '\0'; i++)
+}
+for (k = 0; k < i; k++)
 {
 s3[k] = s1[k];
 }
-s3[k + 1] = '\0';
+while (k < (i + j))
+{
+s3[k] = s2[k - i];
+k++;
 }
+s3[k + 1] = '\0';
 return (s3);
 }
-return (0);
-}
-
