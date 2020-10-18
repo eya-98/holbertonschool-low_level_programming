a7c800f1df2c5e65ca6541d5bd32798fcd15032a
eya-98 <eya.nani@sesame.com.tn>
2020-10-18 01:51:17 +0000

    alloc

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -22,7 +22,14 @@ for (i = 0; i < height; i++)
 {
 s[i] = malloc(width * sizeof(int));
 if (s[i] == 0)
+{
+for (j = 0; j <= i; j++)
+{
 free(s[i]);
+}
+free(s);
+return (0);
+}
 }
 for (i = 0; i < height; i++)
 {
