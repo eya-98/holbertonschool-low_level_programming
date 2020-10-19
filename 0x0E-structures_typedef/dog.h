3a72164c275c61fb3986ae617c4e62d85f7b8caa
eya-98 <eya.nani@sesame.com.tn>
2020-10-19 20:21:35 +0000

    dog structure

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef DJANGO
 #define DJANCO
-typedef struct dog 
+/**
+ *struct dog - structure of dog
+ *@name: dog's name
+ *@age: dog's age
+ *@owner: dog's owner
+ */
+typedef struct dog
 {
 char *name;
 float age;
