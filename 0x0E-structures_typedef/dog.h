6a730a34775487af1d693139cd32671f6cdf2e23
eya-98 <eya.nani@sesame.com.tn>
2020-10-19 21:08:57 +0000

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,4 +12,8 @@ char *name;
 float age;
 char *owner;
 } dog;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
