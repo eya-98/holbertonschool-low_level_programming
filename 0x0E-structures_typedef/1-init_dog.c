44bcb2d30cbed76132f376da45a2eae4490c4a6f
eya-98 <eya.nani@sesame.com.tn>
2020-10-19 21:13:14 +0000

    init var

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -5,13 +5,13 @@
  * @name: char
  * @age: float
  * @owner: char
- * Return: void 
+ * Return: void
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 if (d == 0)
 return;
-else 
+else
 d->name = name;
 d->age = age;
 d->owner = owner;
