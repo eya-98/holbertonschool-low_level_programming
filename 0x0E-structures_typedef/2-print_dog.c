60517c7fc0e05127a179d22b1fb657a0dfe82502
eya-98 <eya.nani@sesame.com.tn>
2020-10-19 22:17:26 +0000

    print dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,8 +14,8 @@ if (d->name == 0)
 printf("Name: (nil)\n");
 else
 printf("Name: %s\n", d->name);
- if (d->age == 0)
-printf("Age: (nil", d->age);
+if (d->age == 0)
+printf("Age: (nil)\n");
 else
 printf("Age: %f\n", d->age);
 if (d->owner == 0)
