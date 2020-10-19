510cbcda91063e200e0a4703a5d11d062366349b
eya-98 <eya.nani@sesame.com.tn>
2020-10-19 21:22:18 +0000

    init dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,6 +1,6 @@
 #include "dog.h"
 /**
- * struct dog - initalize a variable of type struct dog
+ * init_dog - initalize a variable of type struct dog
  * @d: struct dog
  * @name: char
  * @age: float
@@ -9,7 +9,9 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-if (d != 0)
+if (d == 0)
+return;
+else
 d->name = name;
 d->age = age;
 d->owner = owner;
