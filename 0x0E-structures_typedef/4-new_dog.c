f2889aaa69f40810d82b7201855abd3794c92c38
eya-98 <eya.nani@sesame.com.tn>
2020-10-20 14:56:51 +0000

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,34 +10,26 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *d;
-int i, j;
-char *copy_name = '\0';
-char *copy_owner = '\0';
 if (name == 0 || owner == 0)
 return (0);
 d = malloc(sizeof(dog_t));
 if (d == 0)
 return (0);
-for (i = 0; name[i] != '\0'; i++)
-copy_name[i] = name[i];
-for (j = 0; owner[j] != '\0'; j++)
-copy_owner[j] = owner[j];
-d->name = malloc(sizeof(char) * (i + 1));
+d->name = malloc(sizeof(char) * (sizeof(name) + 1));
 if (d->name == 0)
+{
+free(d);
+return (0);
+}
+d->owner = malloc(sizeof(char) * (sizeof(owner) + 1));
+if (d->owner == 0)
 {
 free(d->name);
 free(d);
 return (0);
 }
-d->owner = malloc(sizeof(char) * (j + 1));
-if (d->owner == 0)
-{
-free(d->owner);
-free(d);
-return (0);
-}
-d->name = copy_name;
+d->name = name;
 d->age = age;
-d->owner = copy_owner;
+d->owner = owner;
 return (d);
 }
