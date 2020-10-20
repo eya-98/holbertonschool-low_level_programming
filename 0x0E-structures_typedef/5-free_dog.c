454afd87052dd7b64bbbc37e31bec99f7ddb7311
eya-98 <eya.nani@sesame.com.tn>
2020-10-20 17:28:43 +0000

    free dogs

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog -  a function that frees dogs.
+ * @d : dog-t var.
+ * Return: Always 0.
+ */
+void free_dog(dog_t *d)
+{
+if (d == 0)
+{
+free(d->owner);
+free(d->name);
+free(d);
+}
+}
