81d0d9780179caa77cdaf117f5bea5ba971c1b11
eya-98 <eya.nani@sesame.com.tn>
2020-10-21 23:01:59 +0000

    fixed

---
+++ 0x0F-function_pointers/3-get-op_func.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+ *op_add - add two int
+ *@a: int
+ *@b: int
+ *Return: int
+ */
+int op_add(int a, int b)
+{
+return (a + b);
+}
+/**
+ *op_sub - substract two int
+ *@a: int
+ *@b: int
+ *Return: int
+ */
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+/**
+ *op_mul - multiply two 
+ *@a: int
+ *@b: int
+ *Return: int 
+ */
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+/**
+ *op_div - divise two iny
+ *@a: int
+ *@b: int
+ *Return: iny
+ */
+int op_div(int a, int b)
+{
+return (a / b);
+}
+/**
+ *op_mod - modulo
+ *@a: int
+ *@b: int
+ *Return: int
+ */
+int op_mod(int a, int b)
+{
+return (a % b);
+}
