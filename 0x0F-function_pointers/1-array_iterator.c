5ff6aaa7849c77e07808491e40287d9a3d4fc236
eya-98 <eya.nani@sesame.com.tn>
2020-10-21 15:09:48 +0000

    array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * array_iterator - a function that executes a function
+ * @array : 
+ * @size : size_t
+ * @action : pointer 
+ * Return: Nothing.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+int i;
+if (size != 0 && array != 0 && action != 0)
+for (i = 0; i < size ; i++)
+action(array[i]);
+}
