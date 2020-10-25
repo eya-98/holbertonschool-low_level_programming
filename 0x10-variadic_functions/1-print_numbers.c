6712a92ce77fe7a6fd2121c12a95f8f3ec33992b
eya-98 <eya.nani@sesame.com.tn>
2020-10-25 14:57:55 +0000

    print numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -10,8 +10,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 unsigned int i;
 va_list valist;
-if (n == 0)
-return;
 va_start(valist, n);
 for (i = 0; i < n - 1; i++)
 {
