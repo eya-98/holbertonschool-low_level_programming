3fdd895d0d5c6c04337bdebe765a6a2f1a538975
eya-98 <eya.nani@sesame.com.tn>
2020-11-03 19:49:50 +0000

    print a list of int

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -5,7 +5,7 @@
 
 /**
  * print_listint - prints all the elements of a listint_t list
- * @h data structure
+ * @h : data structure
  * Return: a size_t
  */
 size_t print_listint(const listint_t *h)
@@ -13,7 +13,7 @@ size_t print_listint(const listint_t *h)
 size_t i = 0;
 while (h != NULL)
 {
-printf("%d\n",h->n);
+printf("%d\n", h->n);
 h = h->next;
 i++;
 }
