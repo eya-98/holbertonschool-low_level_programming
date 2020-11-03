3c05f26f1346220a9cb62f6ecb68489e5ed89367
eya-98 <eya.nani@sesame.com.tn>
2020-11-03 22:52:13 +0000

    free list of int

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * free_listint - free a list of int
+ * @h : data structure
+ * Return: void
+ */
+void free_listint(listint_t *head)
+{
+listint_t *new;
+while (head)
+{
+new = head;
+free(new);
+head = head->next;
+}
+}
