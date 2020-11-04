7b6b7aa72f5514a5db064af14d3860f3d0368ecb
eya-98 <eya.nani@sesame.com.tn>
2020-11-04 18:05:38 +0000

    insert node

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * *insert_nodeint_at_index - inserts a new node at a given position.
+ * @head : data structure
+ * @idx : he index of the list where the new node should be added.
+ * @n : integer
+ * Return: Always 0.
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+unsigned int i;
+listint_t *string, *new;
+if (*head == NULL)
+return (0);
+string = *head;
+new = *head;
+for (i = 0; string != '\0'; i++)
+{
+string = string->next;
+}
+if (i < idx)
+return (0);
+for (i = 0; i < idx; i++)
+{
+new = new->next;
+}
+new->n = n;
+for (i = idx + 1; new != '\0'; i++)
+new = new->next;
+return (new);
+}
