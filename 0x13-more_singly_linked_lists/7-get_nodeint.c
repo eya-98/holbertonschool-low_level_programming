fbfdf82366dc54e46580a7311fad2e2dc2d5a83e
eya-98 <eya.nani@sesame.com.tn>
2020-11-04 12:00:08 +0000

    get node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,7 +12,7 @@
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 unsigned int i;
- listint_t *string, *new;
+listint_t *string, *new;
 if (head == '\0')
 return (NULL);
 string = head;
@@ -28,6 +28,6 @@ for (i = 0; i < index; i++)
 if (new == NULL)
 return (NULL);
 new = new->next;
-} 
+}
 return (new);
 }
