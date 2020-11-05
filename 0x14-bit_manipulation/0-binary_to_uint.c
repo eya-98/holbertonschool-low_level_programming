5f67fd6686688aa4c0ee2257fa5a00fbfcc53d3f
eya-98 <eya.nani@sesame.com.tn>
2020-11-05 21:54:25 +0000

    binary to unsigned int

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * binary_to_uint - returns the value of a bit at a given index
+ * @b : const char
+ * Return: an unsigned int
+ */
+unsigned int binary_to_uint(const char *b)
+{
+int i, j, x, c;
+unsigned int n = 0;
+if (b == NULL)
+return (0);
+for (i = 0; b[i] != '\0'; i++)
+{
+if (b[i] != 48 && b[i] != 49)
+return (0);
+}
+j = 1;
+for (c = i - 1; c >= 0; c--)
+{
+x = b[c] - '0';
+x = x *j;
+j = j * 2;
+n = n + x;
+}
+return (n);
+}
