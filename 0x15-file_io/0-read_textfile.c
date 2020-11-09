16d0e05c3bf8befc2bc55de2779a8066ee76e199
eya-98 <eya.nani@sesame.com.tn>
2020-11-09 14:24:39 +0000

    read text file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,8 +7,9 @@
 #include <fcntl.h>
 /**
  * read_textfile - reads a text file and prints it to the POSIX standard output
- * @filename : the file 
+ * @filename : the file
  * @letters : the number of letters it should read and print
+ * Return: a ssize_t
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
@@ -18,14 +19,23 @@ char *buf;
 if (filename == NULL)
 return (0);
 fd = open(filename, O_RDONLY);
-buf = malloc (sizeof(char) * letters);
+buf = malloc(sizeof(char) * letters);
 if (buf == NULL)
 return (0);
 if (fd == 0)
 return (0);
 str = read(fd, buf, letters);
-str2 = write(STDOUT_FILENO, buf, str);  
-close(fd);
+if (str == -1)
+{
 free(buf);
-return(str2);
+return (0);
+}
+str2 = write(STDOUT_FILENO, buf, str);
+if (str2 == -1)
+{
+free(buf);
+return (0);
+}
+free(buf);
+return (str2);
 }
