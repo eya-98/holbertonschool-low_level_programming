281a3e2bb845969f5c11e77861a3cefdb3ab1772
eya-98 <eya.nani@sesame.com.tn>
2020-11-10 09:52:44 +0000

    create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename : the file
+ * @text_content : a NULL terminated string to write to the file
+ * Return: an int
+ */
+int create_file(const char *filename, char *text_content)
+{
+int fd, i;
+if (filename == NULL)
+return (-1);
+fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
+if (fd == -1)
+return (-1);
+i = 0;
+if (text_content[i] == '\0')
+{
+close(fd);
+return (1);
+}
+for (i = 0; text_content[i] != '\0'; i++)
+{
+}
+write(fd, text_content, i);
+close(fd);
+return (1);
+}
