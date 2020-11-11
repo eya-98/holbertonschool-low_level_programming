9a905b3876a4dfdf6c398e1011f41a369fde1236
eya-98 <eya.nani@sesame.com.tn>
2020-11-11 15:18:34 +0000

    print array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -8,12 +8,13 @@
  */
 void print_array(int *a, int n)
 {
-int i = 0;
-while (i < n - 1)
+int i;
+for (i = 0; i < n; i++)
 {
+if (i < n - 1)
 printf("%d, ", a[i]);
+else 
+printf("%d\n", a[i]);
 i++;
 }
-if (i == n)
-printf("%d\n", a[i]);
 }
