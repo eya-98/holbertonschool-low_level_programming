4b8e7180247a82a2920d0a136e0fa6fee3d03e79
eya-98 <eya.nani@sesame.com.tn>
2020-11-15 21:36:27 +0000

    concat string

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,12 +11,12 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 char *s;
-unsigned int f, i, j, l;
+unsigned int m, i, j, l;
 if (s1 == NULL)
 i = 0;
 else
 {
-for (i = 0; s1[i] ; i++)
+for (i = 0; s1[i]; i++)
 {
 }
 }
@@ -24,7 +24,7 @@ if (s2 == NULL)
 j = 0;
 else
 {
-for (j = 0; s2[j] ; j++)
+for (j = 0; s2[j]; j++)
 {
 }
 }
@@ -32,15 +32,11 @@ if (j > n)
 j = n;
 s = malloc(sizeof(char) * (i + j + 1));
 if (s == NULL)
-return (0);
+return (NULL);
 for (l = 0; l < i; l++)
-{
 s[l] = s1[l];
-}
-for (f = 0; f < j; f++)
-{
-s[l + f] = s2[f];
-}
-s[l + f + 1] = '\0';
+for (m = 0; m < j; m++)
+s[m + l] = s2[m];
+s[l + m] = '\0';
 return (s);
 }
