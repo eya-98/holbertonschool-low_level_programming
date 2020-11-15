051b1daae1a4968cc34f3472fb9a18c600bb5261
eya-98 <eya.nani@sesame.com.tn>
2020-11-15 21:50:37 +0000

    calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * *_calloc - allocates memory for an array
+ * @nmemb: number of elements
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+unsigned int i;
+char *s;
+if ((nmemb == 0) || (size == 0))
+return (0);
+for (i = 0; i < nmemb; i++)
+{
+s = malloc(sizeof(char) * size);
+if (s == NULL)
+return (0);
+}
+return (s);
+}
