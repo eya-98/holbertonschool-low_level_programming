4ebc650efa60bee1d8de888f47aafd5a54f47323
eya-98 <eya.nani@sesame.com.tn>
2020-11-15 21:59:45 +0000

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,22 +13,13 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 unsigned int i;
-char **s;
+char *s;
 if ((nmemb == 0) || (size == 0))
 return (0);
-s = malloc(sizeof(char) * nmemb);
+s = malloc(sizeof(char) * nmemb * size);
 if (s == NULL)
 return (0);
-for (i = 0; i < nmemb; i++)
-{
-s[i] = malloc (sizeof(char) * size);
-if (s[i] == NULL)
-{
-free(s);
-return (0);
-}
-}
-for (i = 0; i < nmemb; i++)
+ for (i = 0; i < (nmemb * size); i++)
 {
 s[i] = 0;
 }
