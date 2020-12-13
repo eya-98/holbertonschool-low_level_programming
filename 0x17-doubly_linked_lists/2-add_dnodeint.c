914869b66861695ecb6db5288c931187e2e39e41
eya-98 <eya.nani@sesame.com.tn>
2020-12-13 14:04:01 +0000

    add a node

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ *add_dnodeint - add a node at the beginning of the list
+ *@head : pointer to the list
+ *@n : a number to add
+ *
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+dlistint_t *new;
+new = malloc(sizeof(int));
+if (new == NULL)
+return (0);
+new->n = n;
+new->next = *head;
+*head = new;
+return (*head);
+}
