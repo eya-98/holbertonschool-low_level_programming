6ea4c91c1b89791d7d557acec0cff3d280ca5b2d
eya-98 <eya.nani@sesame.com.tn>
2020-12-13 14:06:14 +0000

    add a node

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -8,7 +8,7 @@
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 dlistint_t *new;
-new = malloc(sizeof(int));
+new = malloc(sizeof(dlistint_t));
 if (new == NULL)
 return (0);
 new->n = n;
