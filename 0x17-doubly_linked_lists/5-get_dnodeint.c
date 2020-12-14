94dbf327f77350c5543d7350d93e79f415952736
eya-98 <eya.nani@sesame.com.tn>
2020-12-14 16:28:24 +0000

    get a node

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,8 +1,9 @@
 #include "lists.h"
 /**
- *
- *
- *
+ *get_dnodeint_at_index - get the nth node
+ *@head: the list
+ *@index: the index of the node to get
+ *Return: the node or null
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
