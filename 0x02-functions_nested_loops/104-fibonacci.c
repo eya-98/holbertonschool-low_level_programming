5b0425bd4b685da2677cecaeb4594d9fba2a9c8b
eya-98 <eya.nani@sesame.com.tn>
2021-01-10 17:20:38 +0000

    fibonacci numbers

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,25 @@
+#include "stdio.h"
+/**
+ *main - finds and prints the first 98 Fibonacci numbers
+ *Return: Null
+ *
+ */
+int main(void)
+{
+unsigned long int i, j, n;
+i = 1;
+j = 2;
+n = 0;
+while (n < 96)
+{
+printf("%lu, ", i);
+printf("%lu, ", j);
+i = i + j;
+j = i + j;
+n++;
+}
+printf("%lu, ", i);
+printf("%lu", j);
+printf("\n");
+return (0);
+}
