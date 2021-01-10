cdf1eb77624597cdcbb3c12bf366915ce1cfb8b1
eya-98 <eya.nani@sesame.com.tn>
2021-01-10 18:12:39 +0000

    print an integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,18 +7,18 @@
 void print_number(int n)
 {
 unsigned int m, i, j;
-if (n == 0)
-_putchar('0');
 if (n < 0)
 {
 _putchar('-');
 n = -n;
 }
-j = 1;
-if (n < 10)
+if (n == 0)
+_putchar('0');
+else if (n < 10)
 _putchar('0' + n);
 else
 {
+j = 1;
 i = n;
 m = 0;
 while (i >= 10)
