7a804900393a42f43f79d648c3e1b60a33d23347
eya-98 <eya.nani@sesame.com.tn>
2021-01-10 15:33:27 +0000

    natural

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- *
- *
+ *natural - prints the sum of all the multiples of 3 or 5 below 1024
+ *Return: the sum
  */
-int main()
+void natural()
 {
-int i, j, sum;
+int i, j, sum, num, k;
 i = 1;
 j = 1;
 sum = 0;
@@ -16,8 +16,21 @@ sum = sum + i;
 }
 while (j < 1024)
 {
-j = j * 3;
+j = j * 5;
 sum = sum + j;
 }
-return (sum);
+k = sum;
+num = 1;
+while (k >= 10)
+{
+num = num * 10;
+k = k / 10;
+}
+while (sum)
+{
+_putchar('0' + sum / num);
+sum = sum - (sum / num);
+}
+_putchar('0' + sum % 10);
+_putchar('\n');
 }
