1ba881c51c4a1daba92de2be37b00c91c59d7f7d
eya-98 <eya.nani@sesame.com.tn>
2021-01-17 21:25:10 +0000

    hash table

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,26 +1,27 @@
 #include "hash_tables.h"
 /**
- **
- *
- *
- *
- *
+ *hash_table_print - prints a hash table
+ *@ht: the hashtable
  */
 void hash_table_print(const hash_table_t *ht)
 {
-unsigned int c, i, j;
+unsigned long int c, i;
 if (!ht)
 return;
 printf("{");
-c = 1;
-for (i = 0; i < ht->size; i++)
-{
-for (j = 0; ht->array[j] != NULL; j++)
-{
-if (c != 1)
-printf(", ");
 c = 0;
-printf("%s': %s", ht->array[j]->key, ht->array[j]->value);
+for (i = 0; i <= ht->size; i++)
+{
+if (ht->array[i] != NULL)
+{
+while (ht->array[i])
+{
+if (c != 0)
+printf(", ");
+c++;
+printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
+ht->array[i] = ht->array[i]->next;
+}
 }
 }
 printf("}\n");
