7916a99ce83601702360dc5d4c4362fce3029327
eya-98 <eya.nani@sesame.com.tn>
2021-01-17 17:27:36 +0000

    hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,8 +1,8 @@
 #include "hash_tables.h"
 /**
- *
- *
- *
+ *hash_table_create - create an empty hash table
+ *@size : size of the table
+ *Return: a pointer to the the newly created hash table
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
@@ -11,7 +11,7 @@ hush = malloc(sizeof(hash_table_t));
 if (hush == NULL)
 return (NULL);
 hush->size = size;
-hush->array = malloc(sizeof(hash_node_t) * size);
+hush->array = malloc(sizeof(hush->array) * size);
 if (hush->array == NULL)
 {
 free(hush);
