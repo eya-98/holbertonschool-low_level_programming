a71a7786139fb1760dd5eb153969387d6fb7761d
eya-98 <eya.nani@sesame.com.tn>
2021-04-25 18:58:10 +0000

    linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,8 +1,9 @@
 #include "search_algos.h"
 /**
-*linear_search: searches for a value  using the Linear search algorithm
+*linear_search - searches for a value  using the Linear search algorithm
 *@array: the array in which we will look fro the element
-*@value: the element 
+*@value: the element
+*@size: length of th array
 *Return: index
 */
 int linear_search(int *array, size_t size, int value)
@@ -13,7 +14,7 @@ for (i = 0; i < size; i++)
 printf("Value checked array[%u] = [%d]\n", i, array[i]);
 if (array[i] == value)
 {
-return i;
+return (i);
 }
 }
 return (-1);
