1eb5e51da72f2b43bb145bb038d1692c0c35f67f
eya-98 <eya.nani@sesame.com.tn>
2021-04-25 22:45:57 +0000

    binary search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,22 +1,37 @@
 #include "search_algos.h"
 /**
-*binary_search:  searches for a value using the Binary search algorithm
+*binary_search - searches for a value using the Binary search algorithm
 *@array: the array in which we will look fro the element
-*@value: the element 
+*@value: the element
+*@size: the length of the array
 *Return: index
 */
 int binary_search(int *array, size_t size, int value)
 {
-unsigned int i=0;
-while(size >= 2)
+int i = 0, left = 0, right = size - 1, middle;
+if (array == NULL)
+return (-1);
+while (left <= right)
 {
-if (array[i] == value)
+printf("Searching in array: ");
+for (i = left; i < right; i++)
 {
-    return i;
+printf("%i, ", array[i]);
+}
+printf("%i\n", array[right]);
+middle = (left + right) / 2;
+if (array[middle] < value)
+{
+left = middle + 1;
+}
+else if (array[middle] > value)
+{
+right = middle - 1;
 }
 else
-    i++;
-    size = size / 2;
+{
+return (middle);
+}
 }
 return (-1);
 }
